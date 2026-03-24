7b903eeaccb5154561165b34a8547c6005befa0a
CRISTIANE ARAUJO SOUZA DOS REIS <>
2026-03-24 10:52:52 -0300

    fazendo a automação do cenário compra pedido

--- QAx/project/startbugs-cucumber/features/pedido.feature
+++ QAx/project/startbugs-cucumber/features/pedido.feature
@@ -13,10 +13,9 @@ Funcionalidade: Pedidos
     E que iniciei a compra do item "Expresso Tradicional"
     Quando faço a busca do seguinte CEP: "04534011"
     E informo os demais dados do endereço:
-    | Numero      | 1000    |
-    | Complemento | Apto 22 |
+    | number     | 1000    |
+    | details    | Apto 22 |
     E escolho a forma de pagamento "Cartão de Débito"
     E por fim finalizo a compra 
     Então sou redirecionado para a página de confirmação de Pedidos
     E deve ser informado um prazo de entrega entre 20 a 30 minutos
-    
--- QAx/project/startbugs-cucumber/features/step_definitions/pedido_steps.rb
+++ QAx/project/startbugs-cucumber/features/step_definitions/pedido_steps.rb
@@ -1,22 +1,22 @@
-Dado('que iniciei a compra do item {string}') do |string|
-  pending # Write code here that turns the phrase above into concrete actions
+Dado('que iniciei a compra do item {string}') do |product_name|
+    @home.buy(product_name) 
 end
 
-Quando('faço a busca do seguinte CEP: {string}') do |string|
-  pending # Write code here that turns the phrase above into concrete actions
+Quando('faço a busca do seguinte CEP: {string}') do |zipcode|
+    @checkout.find_zipcode(zipcode)  
 end
 
 Quando('informo os demais dados do endereço:') do |table|
-  # table is a Cucumber::MultilineArgument::DataTable
-  pending # Write code here that turns the phrase above into concrete actions
+    @checkout.fill_address(table.rows_hash)
 end
 
-Quando('escolho a forma de pagamento {string}') do |string|
-  pending # Write code here that turns the phrase above into concrete actions
+Quando('escolho a forma de pagamento {string}') do |payment_type|
+     # //label//div[text()="À vista no Pix"]
+    @checkout.choice_payment(payment_type)
 end
 
 Quando('por fim finalizo a compra') do
-  pending # Write code here that turns the phrase above into concrete actions
+    @checkout.submit
 end
 
 Então('sou redirecionado para a página de confirmação de Pedidos') do
