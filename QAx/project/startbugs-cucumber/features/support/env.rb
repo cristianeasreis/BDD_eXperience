8cb177dec1e5f05f214bde7522b0896c4a63e726
CRISTIANE ARAUJO SOUZA DOS REIS <>
2026-04-17 18:51:16 -0300

    subindo ajuste

--- QAx/project/startbugs-cucumber/features/support/env.rb
+++ QAx/project/startbugs-cucumber/features/support/env.rb
@@ -1,7 +1,17 @@
 require 'capybara/cucumber'
 require 'selenium-webdriver'
 
+Capybara.register_driver :selenium do |app|
+  options = Selenium::WebDriver::Chrome::Options.new
+  options.add_argument('--no-sandbox')
+  options.add_argument('--disable-dev-shm-usage')
+  options.add_argument('--disable-gpu')
+  options.add_argument('--disable-web-resources')
+  
+  Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
+end
+
 Capybara.configure do |config|
   config.default_driver = :selenium
-  config.default_max_wait_time = 5
+  config.default_max_wait_time = 10
 end
